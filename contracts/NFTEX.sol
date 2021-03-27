d1066d456faf96aa978f4046008907376b0712c0
SolusLumen <74754077+SolusLumen@users.noreply.github.com>
2021-03-27 12:44:53 +0900

    Initial commit

---
+++ contracts/NFTEX.sol
@@ -0,0 +1,230 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract NFTEX is ERC721Holder, Ownable {
+
+  struct Order {
+    uint8 orderType;  //0:Fixed Price, 1:Dutch Auction, 2:English Auction
+    address seller;
+    IERC721 token;
+    uint256 tokenId;
+    uint256 startPrice;
+    uint256 endPrice;
+    uint256 startBlock;
+    uint256 endBlock;
+    uint256 lastBidPrice;
+    address lastBidder;
+    bool isSold;
+  }
+
+  mapping (IERC721 => mapping (uint256 => bytes32[])) public orderIdByToken;
+  mapping (address => bytes32[]) public orderIdBySeller;
+  mapping (bytes32 => Order) public orderInfo;
+
+  address public feeAddress;
+  uint16 public feePercent;
+
+  event MakeOrder(IERC721 indexed token, uint256 id, bytes32 indexed hash, address seller);
+  event CancelOrder(IERC721 indexed token, uint256 id, bytes32 indexed hash, address seller);
+  event Bid(IERC721 indexed token, uint256 id, bytes32 indexed hash, address bidder, uint256 bidPrice);
+  event Claim(IERC721 indexed token, uint256 id, bytes32 indexed hash, address seller, address taker, uint256 price);
+
+
+  constructor(uint16 _feePercent) {
+    require(_feePercent <= 10000, "input value is more than 100%");
+    feeAddress = payable(msg.sender);
+    feePercent = _feePercent;
+  }
+
+
+  // view fx
+  function getCurrentPrice(bytes32 _order) public view returns (uint256) {
+    Order storage o = orderInfo[_order];
+    uint8 orderType = o.orderType;
+    if (orderType == 0) {
+      return o.startPrice;
+    } else if (orderType == 2) {
+      uint256 lastBidPrice = o.lastBidPrice;
+      return lastBidPrice == 0 ? o.startPrice : lastBidPrice;
+    } else {
+      uint256 _startPrice = o.startPrice;
+      uint256 _startBlock = o.startBlock;
+      uint256 tickPerBlock = (_startPrice - o.endPrice) / (o.endBlock - _startBlock);
+      return _startPrice - ((block.number - _startBlock) * tickPerBlock);
+    }
+  }
+
+  function tokenOrderLength(IERC721 _token, uint256 _id) external view returns (uint256) {
+    return orderIdByToken[_token][_id].length;
+  }
+
+  function sellerOrderLength(address _seller) external view returns (uint256) {
+    return orderIdBySeller[_seller].length;
+  }
+
+
+  // make order fx
+  //0:Fixed Price, 1:Dutch Auction, 2:English Auction
+  function dutchAuction(IERC721 _token, uint256 _id, uint256 _startPrice, uint256 _endPrice, uint256 _endBlock) public {
+    require(_startPrice > _endPrice, "End price should be lower than start price");
+    _makeOrder(1, _token, _id, _startPrice, _endPrice, _endBlock);
+  }  //sp != ep
+
+  function englishAuction(IERC721 _token, uint256 _id, uint256 _startPrice, uint256 _endBlock) public {
+    _makeOrder(2, _token, _id, _startPrice, 0, _endBlock);
+  } //ep=0. for gas saving.
+
+  function fixedPrice(IERC721 _token, uint256 _id, uint256 _price, uint256 _endBlock) public {
+    _makeOrder(0, _token, _id, _price, 0, _endBlock);
+  }  //ep=0. for gas saving.
+
+  function _makeOrder(
+    uint8 _orderType,
+    IERC721 _token,
+    uint256 _id,
+    uint256 _startPrice,
+    uint256 _endPrice,
+    uint256 _endBlock
+  ) internal {
+    require(_endBlock > block.number, "Duration must be more than zero");
+
+    //push
+    bytes32 hash = _hash(_token, _id, msg.sender);
+    orderInfo[hash] = Order(_orderType, msg.sender, _token, _id, _startPrice, _endPrice, block.number, _endBlock, 0, address(0), false);
+
+    orderIdByToken[_token][_id].push(hash);
+    orderIdBySeller[msg.sender].push(hash);
+
+    //check if seller has a right to transfer the NFT token. safeTransferFrom.
+    _token.safeTransferFrom(msg.sender, address(this), _id);
+
+    emit MakeOrder(_token, _id, hash, msg.sender);
+  }
+  //start price. end price. compare which is bigger to check if it is Dutch or English. and time.
+
+  function _hash(IERC721 _token, uint256 _id, address _seller) public view returns (bytes32) {
+    return keccak256(abi.encodePacked(block.number, _token, _id, _seller));
+  }
+  //for testing, temporarily it is public not internal.
+
+
+  // take order fx
+
+  //Bids must be at least 5% higher than the previous bid.
+  //If someone bids in the last 5 minutes of an auction, the auction will automatically extend by 5 minutes.
+  function bid(bytes32 _order) payable external {
+    Order storage o = orderInfo[_order];
+    uint256 endBlock = o.endBlock;
+    uint256 lastBidPrice = o.lastBidPrice;
+
+    require(o.orderType == 2, "only for English Auction");
+    require(block.number <= endBlock, "It's over");
+    // require(endBlock != 0, "Canceled order");  //meaningless. if endblock == 0, blocknumber > 0, "it's over" revert.
+    require(o.seller != msg.sender, "Can not bid to your order");
+
+    if (lastBidPrice != 0) {
+      require(msg.value >= lastBidPrice + (lastBidPrice / 20), "low price bid");  //5%
+    } else {
+      require(msg.value >= o.startPrice, "low price bid");
+    }
+
+    if (block.number > endBlock - 20) {  //20blocks = 5 mins in Etherium.
+      o.endBlock = endBlock + 20;
+    }
+
+    payable(o.lastBidder).transfer(lastBidPrice);
+
+    o.lastBidder = msg.sender;
+    o.lastBidPrice = msg.value;
+
+    emit Bid(o.token, o.tokenId, _order, msg.sender, msg.value);
+  }
+
+
+
+
+  function buyItNow(bytes32 _order) payable external {
+    Order storage o = orderInfo[_order];
+    require(o.endBlock > block.number, "It's over");
+    require(o.orderType < 2, "It's a English Auction");
+    require(o.isSold == false, "Already sold");
+
+
+    uint256 currentPrice = getCurrentPrice(_order);
+    require(msg.value >= currentPrice, "price error");
+
+    uint256 fee = currentPrice * feePercent / 10000;
+    payable(o.seller).transfer(currentPrice - fee);
+    payable(feeAddress).transfer(fee);
+    if (msg.value > currentPrice) {
+      payable(msg.sender).transfer(msg.value - currentPrice);
+    }
+    o.isSold = true;
+
+    o.token.safeTransferFrom(address(this), msg.sender, o.tokenId);
+
+    emit Claim(o.token, o.tokenId, _order, o.seller, msg.sender, currentPrice);
+  }
+  //you can pay only ETH for bidding and buying.
+
+
+  function claim(bytes32 _order) external {
+    Order storage o = orderInfo[_order];
+    address seller = o.seller;
+    address lastBidder = o.lastBidder;
+    require(o.isSold == false, "Already sold");
+
+    require(seller == msg.sender || lastBidder == msg.sender, "Access denied");
+    require(o.orderType == 2, "This function is for English Auction");
+    require(block.number > o.endBlock, "Not yet");
+
+    IERC721 token = o.token;
+    uint256 lastBidPrice = o.lastBidPrice;
+
+    uint256 fee = lastBidPrice * feePercent / 10000;
+
+    o.isSold = true;
+
+    payable(seller).transfer(o.lastBidPrice - fee);
+    payable(feeAddress).transfer(fee);
+    token.safeTransferFrom(address(this), lastBidder, o.tokenId);
+
+    emit Claim(token, o.tokenId, _order, seller, lastBidder, lastBidPrice);
+  }
+  //both seller and taker can call this fx in English Auction. Taker(last bidder) might call this fx.
+  //In both DA and FP, buyItNow is followed by claim in a single tx.
+  //3% fee to dev.
+
+
+  function cancelOrder(bytes32 _order) external {
+    Order storage o = orderInfo[_order];
+    require(o.seller == msg.sender, "Access denied");
+    require(o.lastBidPrice == 0, "Bidding exist"); //for EA. but even in DA, FP, seller can withdraw his/her token with his fx.
+    require(o.isSold == false, "Already sold");
+
+    IERC721 token = o.token;
+    uint256 tokenId = o.tokenId;
+
+    o.endBlock = 0;   //0 endBlock means the order is canceled.
+
+    token.safeTransferFrom(address(this), msg.sender, tokenId);
+    emit CancelOrder(token, tokenId, _order, msg.sender);
+  }
+
+
+  function setFeeAddress(address _feeAddress) external onlyOwner {
+    // require(owner == feeAddress, "setFeeAddress: FORBIDDEN");
+    feeAddress = _feeAddress;
+  }
+
+  function updateFeePercent(uint16 _percent) external onlyOwner {
+    require(_percent <= 10000, "input value is more than 100%");
+    feePercent = _percent;
+  }
+
+}
---
+++ test/NFTEX.js
@@ -0,0 +1,443 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFTEX contract", function () {
+
+  let NFTEX, ex, token, Token, owner, addr1, addr2, addr3;
+
+  async function advanceBlockTo(blockNumber) {
+    for (let i = await ethers.provider.getBlockNumber(); i < blockNumber; i++) {
+      await advanceBlock();
+    }
+  }
+
+  async function advanceBlock() {
+    return ethers.provider.send("evm_mine", [])
+  }
+
+  before(async function () {
+    NFTEX = await ethers.getContractFactory("NFTEX");
+    Token = await ethers.getContractFactory("Token");
+
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
+  });
+
+  beforeEach(async function () {
+    ex = await NFTEX.connect(owner).deploy(500);
+    token = await Token.connect(owner).deploy();
+
+    await token.mint(owner.address, 0);
+    await token.mint(addr1.address, 1);
+    await token.mint(addr1.address, 11);
+    await token.mint(addr2.address, 2);
+  });
+
+  describe("Deployment", function () {
+
+    it("Check token balances", async function () {
+      const ownerBalance = await token.balanceOf(owner.address);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      const addr3Balance = await token.balanceOf(addr3.address);
+      expect(ownerBalance).to.equal(1);
+      expect(addr1Balance).to.equal(2);
+      expect(addr2Balance).to.equal(1);
+      expect(addr3Balance).to.equal(0);
+    });
+
+    it("variables and functions about fee", async function () {
+      expect(await ex.feeAddress()).to.equal(owner.address);
+      expect(await ex.feePercent()).to.equal(500);
+
+      await expect(
+        ex.connect(addr1).setFeeAddress(addr1.address)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+
+      await expect(
+        ex.connect(addr1).updateFeePercent(300)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+
+      await ex.setFeeAddress(addr1.address);
+      expect(await ex.feeAddress()).to.equal(addr1.address);
+
+      await ex.updateFeePercent(300);
+      expect(await ex.feePercent()).to.equal(300);
+
+      await expect(
+        ex.updateFeePercent(10300)
+      ).to.be.revertedWith("input value is more than 100%");
+
+    });
+  });
+
+  describe("Make Order", function () {
+    it("Dutch Auction", async function () {
+      await expect(
+        ex.dutchAuction(token.address, 1, 100, 0, 100)
+      ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved");
+
+      await expect(
+        ex.dutchAuction(token.address, 0, 10, 100, 100)
+      ).to.be.revertedWith("End price should be lower than start price");
+
+      await advanceBlockTo("20");
+
+      await expect(
+        ex.dutchAuction(token.address, 0, 100, 0, 10)
+      ).to.be.revertedWith("Duration must be more than zero");
+
+      await token.approve(ex.address, 0);
+
+      await advanceBlockTo("30");
+      await ex.dutchAuction(token.address, 0, 100, 0, 131);
+      let hash = await ex._hash(token.address, 0, owner.address);
+
+      expect(await ex.getCurrentPrice(hash)).to.equal(100);
+      expect(await ex.tokenOrderLength(token.address, 0)).to.equal(1);
+      expect(await ex.sellerOrderLength(owner.address)).to.equal(1);
+
+      expect(await token.balanceOf(owner.address)).to.equal(0);
+
+      await advanceBlockTo("60");
+      expect(await ex.getCurrentPrice(hash)).to.equal(71);  //??
+
+      await advanceBlockTo("131");
+      expect(await ex.getCurrentPrice(hash)).to.equal(0);   //??
+    });
+
+    it("English Auction", async function () {
+      await expect(
+        ex.englishAuction(token.address, 1, 10, 200)
+      ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved");
+
+      await advanceBlockTo("230");
+
+      await expect(
+        ex.englishAuction(token.address, 0, 10, 10)
+      ).to.be.revertedWith("Duration must be more than zero");
+
+      await token.approve(ex.address, 0);
+
+      await ex.englishAuction(token.address, 0, 10, 300);
+      let hash = await ex._hash(token.address, 0, owner.address);
+
+      expect(await ex.getCurrentPrice(hash)).to.equal(10);
+      expect(await ex.tokenOrderLength(token.address, 0)).to.equal(1);
+      expect(await ex.sellerOrderLength(owner.address)).to.equal(1);
+
+      expect(await token.balanceOf(owner.address)).to.equal(0);
+
+      await advanceBlockTo("260");
+      expect(await ex.getCurrentPrice(hash)).to.equal(10);
+    });
+
+    it("Fixed Price", async function () {
+      await expect(
+        ex.fixedPrice(token.address, 1, 50, 300)
+      ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved");
+
+      await advanceBlockTo("310");
+
+      await expect(
+        ex.fixedPrice(token.address, 0, 50, 10)
+      ).to.be.revertedWith("Duration must be more than zero");
+
+      await token.approve(ex.address, 0);
+
+      await ex.fixedPrice(token.address, 0, 50, 350);
+      let hash = await ex._hash(token.address, 0, owner.address);
+
+      expect(await ex.getCurrentPrice(hash)).to.equal(50);
+      expect(await ex.tokenOrderLength(token.address, 0)).to.equal(1);
+      expect(await ex.sellerOrderLength(owner.address)).to.equal(1);
+
+      expect(await token.balanceOf(owner.address)).to.equal(0);
+
+      await advanceBlockTo("330");
+      expect(await ex.getCurrentPrice(hash)).to.equal(50);
+    });
+
+    it("Multiple orders", async function () {
+      await token.approve(ex.address, 0);
+      await token.connect(addr1).approve(ex.address, 1);
+      await token.connect(addr2).approve(ex.address, 2);
+
+      await ex.fixedPrice(token.address, 0, 50, 500);
+      await ex.connect(addr1).englishAuction(token.address, 1, 50, 600);
+      await ex.connect(addr2).dutchAuction(token.address, 2, 100, 50, 550);
+
+      expect(await ex.tokenOrderLength(token.address, 0)).to.equal(1);
+      expect(await ex.tokenOrderLength(token.address, 1)).to.equal(1);
+      expect(await ex.tokenOrderLength(token.address, 2)).to.equal(1);
+      expect(await ex.sellerOrderLength(owner.address)).to.equal(1);
+      expect(await ex.sellerOrderLength(addr1.address)).to.equal(1);
+      expect(await ex.sellerOrderLength(addr2.address)).to.equal(1);
+
+      expect(await token.balanceOf(owner.address)).to.equal(0);
+      expect(await token.balanceOf(addr1.address)).to.equal(1);
+      expect(await token.balanceOf(addr2.address)).to.equal(0);
+
+      await token.connect(addr1).approve(ex.address, 11);
+      await ex.connect(addr1).fixedPrice(token.address, 11, 50, 600);
+
+      expect(await ex.sellerOrderLength(addr1.address)).to.equal(2);
+
+      expect(await token.balanceOf(addr1.address)).to.equal(0);
+      expect(await token.balanceOf(ex.address)).to.equal(4);
+
+    });
+
+  });
+
+  describe("Cancel Order", function () {
+    it("Cancel", async function () {
+      await token.approve(ex.address, 0);
+      await token.connect(addr1).approve(ex.address, 1);
+      await token.connect(addr2).approve(ex.address, 2);
+      await token.connect(addr1).approve(ex.address, 11);
+
+      await ex.fixedPrice(token.address, 0, 50, 500);
+      let hash0 = await ex._hash(token.address, 0, owner.address);
+
+      await ex.connect(addr1).englishAuction(token.address, 1, 20, 600);
+      let hash1 = await ex._hash(token.address, 1, addr1.address);
+
+      await ex.connect(addr2).dutchAuction(token.address, 2, 100, 50, 550);
+      let hash2 = await ex._hash(token.address, 2, addr2.address);
+
+      await ex.connect(addr1).englishAuction(token.address, 11, 20, 400);
+      let hash3 = await ex._hash(token.address, 11, addr1.address);
+
+      await expect(
+        ex.connect(addr3).cancelOrder(hash0)
+      ).to.be.revertedWith("Access denied");
+
+      expect(await token.balanceOf(addr2.address)).to.equal(0);
+      expect(await token.balanceOf(ex.address)).to.equal(4);
+      expect(await token.ownerOf(2)).to.equal(ex.address);
+
+      await ex.connect(addr2).cancelOrder(hash2);
+      expect(await token.balanceOf(addr2.address)).to.equal(1);
+      expect(await token.balanceOf(ex.address)).to.equal(3);
+      expect(await token.ownerOf(2)).to.equal(addr2.address);
+
+      await expect(
+        ex.connect(addr3).buyItNow(hash2)
+      ).to.be.revertedWith("It's over");
+
+      await ex.connect(addr3).bid(hash1, {value : 30});
+
+      await expect(
+        ex.connect(addr1).cancelOrder(hash1)
+      ).to.be.revertedWith("Bidding exist");
+
+      await ex.connect(addr3).buyItNow(hash0, {value : 55});
+
+      await expect(
+        ex.cancelOrder(hash0)
+      ).to.be.revertedWith("Already sold");
+
+      await advanceBlockTo("401");
+      expect(await token.ownerOf(11)).to.equal(ex.address);
+      await expect(ex.bid(hash3)).to.be.revertedWith("It's over");
+      await ex.connect(addr1).cancelOrder(hash3);
+      expect(await token.ownerOf(11)).to.equal(addr1.address);
+    });
+  });
+
+  describe("Bid Order", function () {
+    it("Bid", async function() {
+      await token.approve(ex.address, 0);
+      await token.connect(addr1).approve(ex.address, 1);
+      await token.connect(addr2).approve(ex.address, 2);
+
+      await ex.englishAuction(token.address, 0, 20, 500);
+      let hash0 = await ex._hash(token.address, 0, owner.address);
+
+      await ex.connect(addr1).fixedPrice(token.address, 1, 50, 500);
+      let hash1 = await ex._hash(token.address, 1, addr1.address);
+
+      await ex.connect(addr2).dutchAuction(token.address, 2, 100, 50, 500);
+      let hash2 = await ex._hash(token.address, 2, addr2.address);
+
+      await expect(ex.bid(hash1)).to.be.revertedWith("only for English Auction");
+      await expect(ex.bid(hash2)).to.be.revertedWith("only for English Auction");
+      await expect(ex.bid(hash0)).to.be.revertedWith("Can not bid to your order");
+
+      await token.connect(addr1).approve(ex.address, 11);
+      await ex.connect(addr1).englishAuction(token.address, 11, 20, 540);
+      let hash3 = await ex._hash(token.address, 11, addr1.address);
+
+      await ex.connect(addr1).cancelOrder(hash3);
+      await expect(ex.bid(hash3)).to.be.revertedWith("It's over");
+
+      await expect(ex.connect(addr1).bid(hash0, {value : 10})).to.be.revertedWith("low price bid");
+      await ex.connect(addr1).bid(hash0, {value : 40});
+      await expect(ex.connect(addr2).bid(hash0, {value : 41})).to.be.revertedWith("low price bid");
+      
+      await advanceBlockTo("501");
+      await expect(ex.connect(addr1).bid(hash0, {value : 70})).to.be.revertedWith("It's over");
+
+    });
+
+    it("Auction extension", async function() {
+      await token.connect(addr1).approve(ex.address, 1);
+      await ex.connect(addr1).englishAuction(token.address, 1, 20, 550);
+      let hash = await ex._hash(token.address, 1, addr1.address);
+
+      await expect(() => ex.connect(addr3).bid(hash, {value : 40})).to.changeEtherBalances([addr3, ex], [-40, 40]);
+      await expect(() => ex.connect(addr2).bid(hash, {value : 50})).to.changeEtherBalances([addr2, addr3, ex], [-50, 40, 10]);
+      
+      await advanceBlockTo("540");
+      await ex.bid(hash, {value : 70});
+
+      await advanceBlockTo("551");
+      await ex.connect(addr3).bid(hash, {value : 100});
+
+      await advanceBlockTo("591");
+      await expect(ex.connect(addr2).bid(hash, {value : 130})).to.be.revertedWith("It's over");
+    });
+
+  });
+
+  describe("Claim Order", function () {
+    it("Claim", async function() {
+      await token.approve(ex.address, 0);
+      await token.connect(addr1).approve(ex.address, 1);
+      await token.connect(addr2).approve(ex.address, 2);
+      await token.connect(addr1).approve(ex.address, 11);
+
+      await ex.englishAuction(token.address, 0, 20, 630);
+      let hash0 = await ex._hash(token.address, 0, owner.address);
+
+      await ex.connect(addr1).fixedPrice(token.address, 1, 50, 630);
+      let hash1 = await ex._hash(token.address, 1, addr1.address);
+
+      await ex.connect(addr2).dutchAuction(token.address, 2, 100, 50, 630);
+      let hash2 = await ex._hash(token.address, 2, addr2.address);
+
+      await ex.connect(addr1).englishAuction(token.address, 11, 20, 630);
+      let hash3 = await ex._hash(token.address, 11, addr1.address);
+
+      await ex.connect(addr1).bid(hash0, {value : 40});
+      await ex.connect(addr3).bid(hash3, {value : 30});
+
+      await expect(ex.claim(hash0)).to.be.revertedWith("Not yet");
+
+      await advanceBlockTo("631");
+
+      await expect(ex.connect(addr2).claim(hash0)).to.be.revertedWith("Access denied");
+      await expect(ex.connect(addr1).claim(hash1)).to.be.revertedWith("This function is for English Auction");
+      await expect(ex.connect(addr2).claim(hash2)).to.be.revertedWith("This function is for English Auction");
+      
+      await ex.setFeeAddress(addr3.address);
+
+      expect(await token.ownerOf(0)).to.equal(ex.address);
+      expect(await token.ownerOf(11)).to.equal(ex.address);
+      await expect(() => ex.connect(addr1).claim(hash0)).to.changeEtherBalances([owner, addr3, ex], [38, 2, -40]);
+      expect(await token.ownerOf(0)).to.equal(addr1.address);
+      expect(await token.ownerOf(11)).to.equal(ex.address);
+      await expect(() => ex.connect(addr1).claim(hash3)).to.changeEtherBalances([addr1, addr3, ex], [29, 1, -30]);
+      expect(await token.ownerOf(0)).to.equal(addr1.address);
+      expect(await token.ownerOf(11)).to.equal(addr3.address);
+
+      await expect(ex.claim(hash0)).to.be.revertedWith("Already sold");
+
+    });
+  });
+
+
+  describe("Buy It Now Order", function () {
+    it("BIN_Fixed Price", async function() {
+      await token.approve(ex.address, 0);
+      await token.connect(addr1).approve(ex.address, 1);
+      await token.connect(addr1).approve(ex.address, 11);
+      await token.connect(addr2).approve(ex.address, 2);
+
+      await ex.englishAuction(token.address, 0, 20, 680);
+      let hash0 = await ex._hash(token.address, 0, owner.address);
+
+      await ex.connect(addr1).fixedPrice(token.address, 1, 50, 660);
+      let hash1 = await ex._hash(token.address, 1, addr1.address);
+
+      await ex.connect(addr1).fixedPrice(token.address, 11, 50, 680);
+      let hash2 = await ex._hash(token.address, 11, addr1.address);
+
+      await ex.connect(addr2).fixedPrice(token.address, 2, 60, 680);
+      let hash3 = await ex._hash(token.address, 2, addr2.address);
+
+      await advanceBlockTo("661");
+      await expect(ex.connect(addr2).buyItNow(hash1)).to.be.revertedWith("It's over");
+      await expect(ex.connect(addr2).buyItNow(hash0)).to.be.revertedWith("It's a English Auction");
+
+      await expect(ex.connect(addr2).buyItNow(hash2, {value : 40})).to.be.revertedWith("price error");
+
+      expect(await token.ownerOf(11)).to.equal(ex.address);
+      await expect(() => ex.connect(addr2).buyItNow(hash2, {value : 50})).to.changeEtherBalances([owner, addr1, addr2], [2, 48, -50]);
+      expect(await token.ownerOf(11)).to.equal(addr2.address);
+
+      await expect(ex.connect(addr2).buyItNow(hash2, {value : 40})).to.be.revertedWith("Already sold");
+
+      expect(await token.ownerOf(2)).to.equal(ex.address);
+      await expect(() => ex.connect(addr1).buyItNow(hash3, {value : 77})).to.changeEtherBalances([owner, addr1, addr2], [3, -60, 57]);
+      expect(await token.ownerOf(2)).to.equal(addr1.address);
+
+
+      await ex.connect(addr1).cancelOrder(hash1);
+      await token.connect(addr1).approve(ex.address, 1);
+      await ex.connect(addr1).fixedPrice(token.address, 1, 20, 680);
+      let hash4 = await ex._hash(token.address, 1, addr1.address);
+
+      await expect(() => ex.connect(addr1).buyItNow(hash4, {value : 50})).to.changeEtherBalances([owner, addr1], [1, -1]);
+
+    });
+
+    it("BIN_Dutch Auction", async function() {
+      await token.approve(ex.address, 0);
+      await token.connect(addr1).approve(ex.address, 1);
+      await token.connect(addr1).approve(ex.address, 11);
+      await token.connect(addr2).approve(ex.address, 2);
+
+      await ex.dutchAuction(token.address, 0, 20, 0, 690);
+      let hash0 = await ex._hash(token.address, 0, owner.address);
+
+      await ex.connect(addr1).dutchAuction(token.address, 1, 100, 50, 732);
+      let hash1 = await ex._hash(token.address, 1, addr1.address);
+
+      await ex.connect(addr1).dutchAuction(token.address, 11, 500, 150, 750);
+      let hash2 = await ex._hash(token.address, 11, addr1.address);
+
+      expect(await ethers.provider.getBlockNumber()).to.equal(684);
+      await ex.connect(addr2).dutchAuction(token.address, 2, 300, 150, 734);
+      let hash3 = await ex._hash(token.address, 2, addr2.address);
+
+      await advanceBlockTo("691");
+      await expect(ex.connect(addr2).buyItNow(hash0)).to.be.revertedWith("It's over");
+
+      await expect(ex.connect(addr2).buyItNow(hash1, {value : 50})).to.be.revertedWith("price error");
+
+      await advanceBlockTo("702");
+      expect(await token.ownerOf(1)).to.equal(ex.address);
+      // expect(await ex.getCurrentPrice(hash1)).to.equal(81);
+      await expect(() => ex.connect(addr2).buyItNow(hash1, {value : 80})).to.changeEtherBalances([owner, addr1, addr2], [4, 76, -80]);
+      expect(await token.ownerOf(1)).to.equal(addr2.address);
+
+      await expect(ex.connect(addr3).buyItNow(hash1, {value : 90})).to.be.revertedWith("Already sold");
+
+      expect(await token.ownerOf(2)).to.equal(ex.address);
+      expect(await ethers.provider.getBlockNumber()).to.equal(704);
+      expect(await ex.getCurrentPrice(hash3)).to.equal(243);                          //??
+      await expect(() => ex.connect(addr1).buyItNow(hash3, {value : 250})).to.changeEtherBalances([owner, addr1, addr2], [12, -240, 228]);
+      expect(await token.ownerOf(2)).to.equal(addr1.address);
+
+      expect(await token.ownerOf(11)).to.equal(ex.address);
+      expect(await ex.getCurrentPrice(hash2)).to.equal(395);                          //??
+      await expect(() => ex.connect(addr1).buyItNow(hash2, {value : 400})).to.changeEtherBalances([owner, addr1], [19, -19]);
+      expect(await token.ownerOf(11)).to.equal(addr1.address);
+      });
+
+  });
+
+  });
+      // expect(await ethers.provider.getBlockNumber()).to.equal(600);
+//expect(await (await web3.eth.getBalance(addr1)).to.equal(10000)); how to check exact ETHbalance
